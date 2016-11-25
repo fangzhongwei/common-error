821f66952ba84072e8fdb8f26b013f8897fc8914
fangzhongwei <zhongwei.fang@56qq.com>
2016-11-25 15:33:57 +0800

    first commit

--- src/main/scala/com/lawsofnature/common/exception/ServiceErrorCode.scala
+++ src/main/scala/com/lawsofnature/common/exception/ServiceErrorCode.scala
@@ -8,6 +8,7 @@ object ServiceErrorCode extends Enumeration {
 
   val EC_SYSTEM_ERROR = Value(10000001, "系统目前无法处理您的请求，请稍后再试。")
   val EC_INVALID_REQUEST = Value(10000002, "不正确的请求，请检查输入。")
+
   val EC_UC_USERNAME_LENGTH_LIMIT = Value(10001001, "用户名长度不能超过64位，请修改或重新输入。")
   val EC_UC_INVALID_MOBILE = Value(10001002, "手机号格式不正确，请修改或重新输入。")
   val EC_UC_EMAIL_LENGTH_LIMIT = Value(10001003, "邮箱长度不能超过64位，请修改或重新输入。")
