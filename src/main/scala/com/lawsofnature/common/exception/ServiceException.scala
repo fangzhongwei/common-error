3dcf069b8c15698ad8f22a6a61e3c7303f8c5079
fangzhongwei <zhongwei.fang@56qq.com>
2016-11-29 17:04:12 +0800

    error cache

--- src/main/scala/com/lawsofnature/common/exception/ServiceException.scala
+++ src/main/scala/com/lawsofnature/common/exception/ServiceException.scala
@@ -5,6 +5,25 @@ import com.lawsofnature.common.exception.ServiceErrorCode.ServiceErrorCode
 /**
   * Created by fangzhongwei on 2016/11/5.
   */
-class ServiceException(val serviceErrorCode: ServiceErrorCode.ServiceErrorCode) extends RuntimeException{
-  def apply(serviceErrorCode: ServiceErrorCode): ServiceException = new ServiceException(serviceErrorCode)
+case class ServiceException private(message: String) extends RuntimeException(message) {
+  private var errorCode: ServiceErrorCode = _
+
+  def make(serviceErrorCode: ServiceErrorCode): ServiceException = {
+     ServiceException.get(serviceErrorCode) match {
+       case Some(exception) => exception
+       case None =>
+         val serviceException: ServiceException = new ServiceException(serviceErrorCode.toString)
+         serviceException.errorCode = serviceErrorCode
+         ServiceException.put(serviceErrorCode, serviceException)
+         serviceException
+     }
+  }
+}
+
+object ServiceException {
+  private val ERROR_MAP: scala.collection.mutable.Map[ServiceErrorCode, ServiceException] = scala.collection.mutable.Map[ServiceErrorCode, ServiceException]()
+
+  private def get(serviceErrorCode: ServiceErrorCode): Option[ServiceException] = ERROR_MAP.get(serviceErrorCode)
+
+  private def put(serviceErrorCode: ServiceErrorCode, serviceException: ServiceException): Unit = ERROR_MAP += (serviceErrorCode -> serviceException)
 }
