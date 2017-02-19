f7fdab4dcdeed279b6ba14a1aa435f8e7b8671c2
fangzhongwei <zhongwei.fang@56qq.com>
2017-02-19 14:22:14 +0800

    error cache

--- src/main/scala/com/jxjxgo/common/exception/ErrorCode.java
+++ src/main/scala/com/jxjxgo/common/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
     EC_INVALID_REQUEST("EC_INVALID_REQUEST", "不正确的请求，请检查输入。"),
 
     EC_UC_INVALID_MOBILE("EC_UC_INVALID_MOBILE", "手机号格式不正确，请修改或重新输入。"),
-    EC_UC_MEMBER_ACCOUNT_FREEZE("EC_UC_MEMBER_ACCOUNT_FREEZE", "您的账户已冻结,请联系客服。"),
+    EC_UC_MEMBER_ACCOUNT_FREEZE("EC_UC_MEMBER_ACCOUNT_FREEZE", "您的账户已冻结。"),
     EC_UC_MEMBER_NOT_EXISTS("EC_UC_MEMBER_NOT_EXISTS", "会员信息未找到。"),
 
     EC_SSO_SESSION_EXPIRED("EC_SSO_SESSION_EXPIRED", "会话已过期，请重新登录。"),
@@ -19,7 +19,7 @@ public enum ErrorCode {
 
     EC_GC_CONFIG_ERROR("EC_GC_CONFIG_ERROR", "系统配置错误。"),
     EC_SMS_TYPE_CONFIG_ERROR("EC_SMS_TYPE_CONFIG_ERROR", "信息类型配置错误。"),
-    EC_SMS_DAY_LIMIT_ERROR("EC_SMS_DAY_LIMIT_ERROR", "一个手机号每天最多获取3次验证码。"),
+    EC_SMS_DAY_LIMIT_ERROR("EC_SMS_DAY_LIMIT_ERROR", "手机号当天获取验证码次数超过系统限制。"),
     EC_SMS_SEND_TOO_OFTEN_ERROR("EC_SMS_SEND_TOO_OFTEN_ERROR", "验证码发送过于频繁。"),
 
     EC_SMS_WRONG_CODE("EC_SMS_WRONG_CODE", "验证码不正确。"),
